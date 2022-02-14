1c0820d5dcad3691581fabf79b36cf2b5c50d52b
Oknaa <mohammadlaadidaoui@gmail.com>
2022-02-14 22:43:11 +0100

    Added camera and player rotation

--- FlatLand/Assets/Scripts/MovementController.cs
+++ FlatLand/Assets/Scripts/MovementController.cs
@@ -1,18 +1,27 @@
+using System;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float sprintSpeed;
+    [SerializeField] private float rotationSpeed;
     private Vector3 _direction;
+    private Vector3 _newDirection;
     private float _speed;
     
+    private void Start() {
+        _direction = Vector3.forward;
+    }
+
     private void Update() {
-        _direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        _newDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         _speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
 
-        transform.Translate(_direction * _speed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_newDirection), rotationSpeed);
         
-        _direction = Vector3.zero;
+        transform.Translate(_newDirection * _speed * Time.deltaTime, Space.World);
+        _direction = _newDirection;
+
         Debug.Log("Horiz: " + Input.GetAxis("Horizontal") + " .. Vertic: " + Input.GetAxis("Vertical"));
     }
 }   
